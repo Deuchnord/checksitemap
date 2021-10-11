dfebe71ba0d1e80b7cd876ee03b99c0b81b31b2c
Jérôme Tanghe <jerome.tanghe@les-tilleuls.coop>
2021-10-11 14:24:24 +0200

    Refactor and make validation more robust

--- checksitemap.py
+++ checksitemap.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
-import sys
+from argparse import ArgumentParser
 from urllib.parse import urlparse
 import requests
 import xml.etree.ElementTree as ET
+
+XML_NAMESPACE = 'http://www.sitemaps.org/schemas/sitemap/0.9'
+XML_ETREE_NAMESPACE = '{%s}' % XML_NAMESPACE
 
 
 def main(url: str) -> int:
@@ -33,29 +36,58 @@ def check_sitemap(sitemap) -> (int, int):
     n_urls = 0
     n_passed = 0
 
+    if not sitemap.tag.startswith(XML_ETREE_NAMESPACE):
+        print('Warning: missing XML namespace on <urlset> tag: please add xmlns="%s"' % XML_NAMESPACE)
+
     for item in sitemap:
         n_urls += 1
 
-        if item.tag != '{http://www.sitemaps.org/schemas/sitemap/0.9}url':
-            print('Error: invalid tag %s, expected <url>' % item.tag)
+        if item.tag.replace(XML_ETREE_NAMESPACE, '') != 'url':
+            print('Error: invalid tag <%s>, expected <url>' % item.tag.replace(XML_ETREE_NAMESPACE, ''))
             continue
 
-        location = item.find('{http://www.sitemaps.org/schemas/sitemap/0.9}loc')
-
-        if location is None:
-            print("Error: missing mandatory <loc> tag for URL number %d!" % n_urls)
-            continue
-
-        r = requests.get(location.text)
-        if r.status_code >= 300:
-            print('Location "%s" returned %d status code' % (location.text, r.status_code))
-            continue
-
-        n_passed += 1
+        if is_url_correct(item, n_urls):
+            n_passed += 1
 
     return n_urls, n_passed
 
 
+def is_url_correct(url, n_url) -> bool:
+    valid = True
+    has_loc = False
+
+    for prop in url:
+        if prop.tag.replace(XML_ETREE_NAMESPACE, '') not in ['loc', 'lastmod', 'changefreq', 'priority']:
+            print('Error: invalid <%s> tag for URL n°%d!' % (prop.tag.replace(XML_ETREE_NAMESPACE, ''), n_url))
+
+            return False
+
+        if prop.tag.replace(XML_ETREE_NAMESPACE, '') == 'loc':
+            has_loc = True
+            loc = prop.text
+            r = requests.get(loc)
+            if r.status_code >= 300:
+                print('Location "%s" returned %d status code' % (loc, r.status_code))
+                valid = False
+
+    if not has_loc:
+        print('Error: URL n°%d has no mandatory <loc> tag!' % n_url)
+        return False
+
+    return valid
+
+
 if __name__ == "__main__":
-    exit(main(sys.argv[1]))
+    try:
+        args = ArgumentParser(
+            description="Validate a sitemap."
+        )
+
+        args.add_argument("sitemap_location", type=str, help="the URL to the sitemap")
+
+        args = args.parse_args()
+
+        exit(main(args.sitemap_location))
+    except KeyboardInterrupt:
+        exit(-1)
 
